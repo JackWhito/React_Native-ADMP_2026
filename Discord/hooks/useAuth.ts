15a3460a86833ad9a88603018bafc52b542a779e
JackWhito <namtrung.work@gmail.com>
2026-03-12 07:40:09 +0700

    feat: Refactor useAuth to utilize apiWithAuth for authenticated requests

--- Discord/hooks/useAuth.ts
+++ Discord/hooks/useAuth.ts
@@ -3,11 +3,11 @@ import { useApi } from "@/lib/axios";
 import { useMutation } from "@tanstack/react-query";
 
 export const useAuthCallback = () => {
-    const api = useApi();
+    const {apiWithAuth} = useApi();
 
     const result = useMutation({
         mutationFn: async () => {
-            const {data} = await api.post("/auth/callback");
+            const {data} = await apiWithAuth({method:"POST", url:"auth/callback"});
             return data;
         }
     })
